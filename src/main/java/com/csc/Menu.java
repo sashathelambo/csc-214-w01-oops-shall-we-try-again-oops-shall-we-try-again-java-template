c902c9eee54b8b8fd1b918d06187eb9a1ab6d07b
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-11-01 13:35:58 -0500

    Update Menu.java

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -4,80 +4,40 @@ import java.util.Scanner;
 
 public class Menu {
 
-    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Test cases for the promptForValue function
+        Integer value1 = promptForValue("Please enter a value between 0 and 50", "Your value is invalid", 0, 50, 25);
+        if (value1 != null) {
+            System.out.println("The value chosen by the user is " + value1);
+        } else {
+            System.out.println("The user aborted the input.");
+        }
+    }
 
-    public static Integer prompt(Integer lowerBound, Integer upperBound, Integer defaultValue, String promptMessage, String errorMessage) {
-        Integer userValue;
-
-        while (true) {
-            String modifiedPrompt = promptMessage;
-            if (defaultValue != null) {
-                modifiedPrompt += " Enter 'default' to use the default value of " + defaultValue + ".";
-            }
-            modifiedPrompt += " Enter 'exit' to leave the menu.";
-
-            System.out.println(modifiedPrompt);
-
-            if (!scanner.hasNextLine()) {
-                return null;
-            }
-            
-            String userInput = scanner.nextLine();
-
-            if (userInput.equalsIgnoreCase("default") && defaultValue != null) {
-                return defaultValue;
-            }
-
-            if (userInput.equalsIgnoreCase("exit")) {
-                return null;
-            }
-
-            try {
-                userValue = Integer.parseInt(userInput);
-
-                if ((lowerBound == null || userValue >= lowerBound) && 
-                    (upperBound == null || userValue <= upperBound)) {
-                    return userValue;
+    public static Integer promptForValue(String prompt, String errorMessage, int lowerBound, int upperBound, int defaultValue) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Integer value = null;
+            while (true) {
+                System.out.println(prompt + " (or type 'default' to use the default value: " + defaultValue + ", or 'exit' to abort)");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("exit")) {
+                    return null;
+                } else if (input.isEmpty() || input.equalsIgnoreCase("default")) {
+                    value = defaultValue;
+                    break;
+                } else if (input.matches("\\d+")) {
+                    value = Integer.parseInt(input);
+                    if (value >= lowerBound && value <= upperBound) {
+                        break;
+                    } else {
+                        System.out.println(errorMessage);
+                    }
                 } else {
                     System.out.println(errorMessage);
                 }
-            } catch (NumberFormatException e) {
-                System.out.println(errorMessage);
             }
+            return value;
         }
     }
- 
-    public static void main(String[] args) {
-        try {
-            Integer value1 = prompt(10, 50, 25, "Please enter a value between 10 and 50:", "Your value is invalid. Please try again.");
-            if (value1 != null) {
-                System.out.println("The value chosen by the user is " + value1);
-            } else {
-                System.out.println("The user exited the menu without choosing a value.");
-            }
-
-            Integer value2 = prompt(null, 100, null, "Please enter a value less than or equal to 100:", "Your value is invalid. Please try again.");
-            if (value2 != null) {
-                System.out.println("The value chosen by the user is " + value2);
-            } else {
-                System.out.println("The user exited the menu without choosing a value.");
-            }
-
-            Integer value3 = prompt(-10, null, 0, "Please enter a value greater than or equal to -10:", "Your value is invalid. Please try again.");
-            if (value3 != null) {
-                System.out.println("The value chosen by the user is " + value3);
-            } else {
-                System.out.println("The user exited the menu without choosing a value.");
-            }
-
-            Integer value4 = prompt(null, null, null, "Please enter any integer value:", "Your value is invalid. Please try again.");
-            if (value4 != null) {
-                System.out.println("The value chosen by the user is " + value4);
-            } else {
-                System.out.println("The user exited the menu without choosing a value.");
-            }
-        } finally {
-            scanner.close();
-        }
-    }
+    
 }
